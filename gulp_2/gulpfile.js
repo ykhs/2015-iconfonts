107c42e6fb1411b2d5c23361f22aa13c1a8dcff0
ykhs <ykhs@users.noreply.github.com>
2016-11-07 15:40:59 +0900

    update gulp_2 demo (#1)
    * update modules
    
    * use glyphs event intead of codepoints
    
    https://github.com/nfroidure/gulp-iconfont/commit/44baee9dce124802b2d5ccff6d1f7b10e250392d
    
    * use prependUnicode intend of appendCodepoints
    
    https://github.com/nfroidure/gulp-iconfont/commit/d151b72ca5bf786ea870a01baf2c3b3915889e3a
    
    * update css template
    
    https://github.com/nfroidure/gulp-iconfont/commit/f6ef5f1d3effbf5f5dc9dbf41f61e26cf9acffa2
    
    * update making css
    
    https://github.com/nfroidure/gulp-iconfont/commit/fb8d744d2630376f39f9e0fd9074bc5d501a89f9

--- gulp_2/gulpfile.js
+++ gulp_2/gulpfile.js
@@ -1,31 +1,39 @@
 'use strict';
 
+var async        = require( 'async' );
 var gulp         = require( 'gulp' );
 var iconfont     = require( 'gulp-iconfont' );
 var consolidate  = require( 'gulp-consolidate' );
 
 var fontName = 'myfont';
 
-gulp.task( 'iconfont', function () {
+gulp.task( 'iconfont', function ( done ) {
 
-  gulp.src( [ './src/icons/*.svg' ] )
-  .pipe( iconfont( {
-    fontName: fontName,
-    appendCodepoints: true
-  } ) )
-  .on( 'codepoints', function( codepoints, options ) {
+  var iconStream = gulp.src( [ './src/icons/*.svg' ] )
+    .pipe( iconfont( {
+      fontName: fontName,
+      prependUnicode: true
+    } ) );
 
-      // CSS templating, e.g.
-      gulp.src('./src/icons/icon.css')
-      .pipe(consolidate( 'underscore', {
-        glyphs: codepoints,
-        fontName: fontName,
-        fontPath: '../fonts/',
-        prefix: 'icon'
-      } ) )
-      .pipe( gulp.dest( './build/css/' ) );
-
-  } )
-  .pipe( gulp.dest( './build/fonts/' ) );
+  async.parallel( [
+    function handleGlyphs ( cb ) {
+      iconStream.on( 'glyphs', function ( glyphs ) {
+        gulp.src( './src/icons/icon.css' )
+          .pipe( consolidate( 'underscore', {
+            glyphs: glyphs,
+            fontName: fontName,
+            fontPath: '../fonts/',
+            prefix: 'icon'
+          } ) )
+          .pipe( gulp.dest( './build/css/' ) )
+          .on( 'finish', cb );
+      });
+    },
+    function handleFonts ( cb ) {
+      iconStream
+        .pipe( gulp.dest( './build/fonts/' ) )
+        .on( 'finish', cb );
+    }
+  ], done );
 
 } );
